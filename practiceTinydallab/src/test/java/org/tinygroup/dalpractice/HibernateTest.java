bed179b312be3faaa9c65e27891b289b7436becb
gauge2009 <promvc@live.com>
2016-12-26 12:32:33 +0800

    https://github.com/gauge2009/com.alaya35.acv.git

--- practiceTinydallab/src/test/java/org/tinygroup/dalpractice/HibernateTest.java
+++ practiceTinydallab/src/test/java/org/tinygroup/dalpractice/HibernateTest.java
@@ -19,19 +19,19 @@ public class HibernateTest extends BaseTest {
 
 		assertNotNull(customDo.getId());
 
-//		customDo.setName("hibernate1");
-//		customDo.setAge(12);
-//		int affect = customDao.updateCustom(customDo);
-//		assertEquals(1, affect);
-//
-//		CustomDo customDo2 = customDao.getCustomById(customDo.getId());
-//		assertEquals(12, customDo2.getAge());
-//		assertEquals("hibernate1", customDo2.getName());
-//
-//		List<CustomDo> customs = customDao.getAllCustoms();
-//		assertEquals(1, customs.size());
-//
-//		affect=customDao.deleteCustomById(customDo.getId());
-//		assertEquals(1, affect);
+		customDo.setName("hibernate1");
+		customDo.setAge(12);
+		int affect = customDao.updateCustom(customDo);
+		assertEquals(1, affect);
+
+		CustomDo customDo2 = customDao.getCustomById(customDo.getId());
+		assertEquals(12, customDo2.getAge());
+		assertEquals("hibernate1", customDo2.getName());
+
+		List<CustomDo> customs = customDao.getAllCustoms();
+		assertEquals(1, customs.size());
+
+		affect=customDao.deleteCustomById(customDo.getId());
+		assertEquals(1, affect);
 	}
 }
